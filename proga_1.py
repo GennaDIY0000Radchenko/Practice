5f2526cc5d0267d968a0f7a01c95b1d152f218a1
GennaDIY0000Radchenko <72203137+GennaDIY0000Radchenko@users.noreply.github.com>
2020-10-08 16:10:22 +0300

    Add files via upload

---
+++ proga_1.py
@@ -0,0 +1,19 @@
+# Heshteg
+name=input('Plese , enter name')
+surname=input('Plese , enter surname')
+phone=input('Plese , enter phone number')
+street=input('Plese , enter street name')#Один із блоків введення даних
+house=input('Plese , enter house number')
+apartment=input('Plese , enter apartment number')
+city=input('Plese , enter city number')
+index=input('Plese , enter index number')
+country=input('Plese , enter country name')
+print(name , surname)#Блок виведення даних
+'\n'
+print(phone)#Блок виведення даних
+'\n'
+print('Str.',street,house,'ap.',apartment,city)#Блок виведення даних
+'\n'
+print(index)#Блок виведення даних
+'\n'
+print(country)#Блок виведення даних
